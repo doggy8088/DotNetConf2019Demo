b2715cc599ba94b5b59fdd70f148bb910fe311fc
Will <doggy.huang@gmail.com>
2019-11-09 13:52:05 +0800

    加入 gRPC 範例的 Service 與 Client 實作

---
+++ grpc_client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using grpc1;
+
+namespace grpc_client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client =  new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                                new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+        }
+    }
+}
